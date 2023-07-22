d3df0ca7037cd7075e3e52673e423361d14de45b
ToxToxx <107263231+ToxToxx@users.noreply.github.com>
2023-07-22 15:11:13 +0300

    Добавьте файлы проекта.

---
+++ OOPPracticComputerClub/Program.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPPracticComputerClub
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ComputerClub computerClub = new ComputerClub(8);
+            computerClub.Work();
+        }
+    }
+
+    class ComputerClub
+    {
+        private int _money = 0;
+        private List<Computer> _computers = new List<Computer>();//всегда private в кооллекциях
+        private Queue<Client> _clients = new Queue<Client>();
+
+        public ComputerClub(int computersCount) 
+        {
+            Random random = new Random();
+            
+            for (int i = 0; i < computersCount; i++)
+            {
+                _computers.Add(new Computer(random.Next(5, 15)));
+            }
+
+            CreateNewClients(25, random);
+        }
+
+        public void CreateNewClients(int clientsCount, Random random)//если бы мы создавали ещё один рэндом, то была бы вероятность совпадения значений
+        {
+            for(int i = 0; i < clientsCount;i++)
+            {
+                _clients.Enqueue(new Client(random.Next(100, 250), random));
+            }
+        }
+
+        public void Work()
+        {
+            while (_clients.Count > 0)
+            {
+                Client newClient = _clients.Dequeue();
+                Console.WriteLine($"Баланс компьютерного клуба: {_money} руб. Ждем нового клиента");
+                Console.WriteLine($"У вас новый клиент, и он хочет купить {newClient.DesiredMinutes} минут");
+                ShowAllComputerStates();
+
+                Console.Write("\nВы предлагаете ему компьютер под номером: ");
+                string userInput = Console.ReadLine();
+                
+                if(int.TryParse(userInput, out int computerNumber))
+                {
+                    computerNumber -= 1;
+
+                    if(computerNumber >= 0 && computerNumber < _computers.Count)
+                    {
+                        if (_computers[computerNumber].IsTaken)
+                        {
+                            Console.WriteLine("КОМПЬЮТЕР ЗАНЯТ, КЛИЕНТ РАЗОЗЛИЛСЯ И УШЁЛ");
+                        }
+                        else
+                        {
+                            if (newClient.CheckSolvency(_computers[computerNumber]))
+                            {
+                                Console.WriteLine("Клиент сел за компьютер " + (computerNumber + 1));
+                                _money += newClient.Pay();
+                                _computers[computerNumber].BecomeTaken(newClient);
+                            }
+                            else
+                            {
+                                Console.WriteLine("У клиента денег нет. Он ушёл");
+                            }
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("ВЫ НЕ ЗНАЛИ КУДА ПОСАДИТЬ КЛИЕНТА И ОН РАЗОЗЛИЛСЯ И УШЁЛ");
+                    }
+                }
+                else
+                {
+                    CreateNewClients(1, new Random());
+                    Console.WriteLine("Неверный ввод! Попробуйте снова.");
+                }
+
+                Console.WriteLine("Чтобы перейти к следующему клиенту нажмите любую клавишу");
+                Console.ReadKey();
+                Console.Clear();
+                SpendOneMinute();
+            }
+        }
+
+        private void ShowAllComputerStates()
+        {
+            Console.WriteLine("\n Список всех компьютеров: ");
+            for (int i = 0; i < _computers.Count; i++)
+            {
+                Console.Write(i + 1 + " - ");
+                _computers[i].ShowState();
+            }
+        }
+
+        private void SpendOneMinute()
+        {
+            foreach(var computer in _computers)
+            {
+                computer.SpendOneMinute();
+            }
+        }
+     }
+
+    class Computer
+    {
+        private Client _client;
+        private int _minutesRemaining;
+        public bool IsTaken
+        {
+            get
+            {
+                return _minutesRemaining > 0;
+            }
+        }
+
+        public int PricePerMinute { get; private set; }
+
+        public Computer(int pricePerMinute)
+        {
+            PricePerMinute = pricePerMinute;
+        }
+
+        public void BecomeTaken(Client client)//делать так не нужно лишь для примера
+        {
+            _client = client;//так тоже
+            _minutesRemaining = _client.DesiredMinutes;
+        }
+
+        public void BecomeEmpty()
+        {
+            _client = null;
+        }
+
+        public void SpendOneMinute()
+        {
+            _minutesRemaining--;
+        }
+
+        public void ShowState()
+        {
+            if (IsTaken)
+                Console.WriteLine($"Компьютер занят, осталось минут: {_minutesRemaining}");
+            else
+                Console.WriteLine($"Компьютер свободен, цена за минуту: {PricePerMinute}");
+        }
+    }
+
+    class Client
+    {
+        private int _money;
+        private int _moneyToPay;
+
+        public int DesiredMinutes {  get; private set; }
+
+        public Client(int money, Random random) 
+        {
+            _money = money;
+            DesiredMinutes = random.Next(10, 30);
+        }
+
+        public bool CheckSolvency(Computer computer)
+        {
+            _moneyToPay = computer.PricePerMinute * DesiredMinutes;
+            if(_money >= _moneyToPay)
+            {
+                return true;
+            }
+            else
+            {
+                _moneyToPay = 0;
+                return false;
+            }
+        }
+
+        public int Pay()
+        {
+            _money -= _moneyToPay;
+            return _moneyToPay;
+        }
+    }
+}
